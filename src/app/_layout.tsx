dbe15205455b5673149a9f66063bd7b5d51bcffe
gabriel.vargas <gabriel.vargas@paripassu.com.br>
2025-05-02 16:33:15 -0300

    versao 0.6.0 - Inicio ministerios

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,34 +7,11 @@ import {ActivityIndicator} from "react-native-paper";
 import {View} from "react-native";
 
 export default function Layout() {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const checkAuth = async () => {
-            const token = await AsyncStorage.getItem("token");
-
-            if (!token) {
-                router.replace("/login"); // 🔥 Redireciona se não houver token
-            }
-
-            setLoading(false);
-        };
-
-        checkAuth();
-    }, []);
-
-    if (loading) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="large" color="#0000ff" />
-            </View>
-        );
-    }
-
     return (
         <ThemeProvider>
             <Stack>
                 <Stack.Screen name="login" options={{headerShown: false}}/>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
             </Stack>
         </ThemeProvider>
